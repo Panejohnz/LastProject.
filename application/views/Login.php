e5b9dc17de5735f8fff3b79a9e6a7918fd76bb43
suriyakarn <47375460+suriyakarn@users.noreply.github.com>
2020-01-09 14:45:21 +0700

    .

--- application/views/Login.php
+++ application/views/Login.php
@@ -7,24 +7,24 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>../assets/bluesky/images/goldd.png">
+	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>./assets/bluesky/images/goldd.png">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/vendor/select2/select2.min.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/css/util.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>../assets/logincss/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/css/util.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>./assets/logincss/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -69,21 +69,21 @@
 	</div>
 	
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/vendor/jquery/jquery-3.2.1.min.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/jquery/jquery-3.2.1.min.js');?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/vendor/animsition/js/animsition.min.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/animsition/js/animsition.min.js');?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/vendor/bootstrap/js/popper.js');?>"></script>
-	<script src="<?php echo base_url('../assets/logincss/vendor/bootstrap/js/bootstrap.min.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/bootstrap/js/popper.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/bootstrap/js/bootstrap.min.js');?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/vendor/select2/select2.min.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/select2/select2.min.js');?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/vendor/daterangepicker/moment.min.js');?>"></script>
-	<script src="<?php echo base_url('../assets/logincss/vendor/daterangepicker/daterangepicker.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/daterangepicker/moment.min.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/daterangepicker/daterangepicker.js');?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/vendor/countdowntime/countdowntime.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/vendor/countdowntime/countdowntime.js');?>"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url('../assets/logincss/js/main.js');?>"></script>
+	<script src="<?php echo base_url('./assets/logincss/js/main.js');?>"></script>
 
 
 	
